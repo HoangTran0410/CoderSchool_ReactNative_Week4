6f5c504b717d669f62206b7d6b248ae19f7b80d2
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-08-11 23:09:11 +0700

    add global colors

--- TodoList/constants/Colors.js
+++ TodoList/constants/Colors.js
@@ -1,25 +1,52 @@
 const tintColor = '#2f95dc';
 
 export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
+	tintColor,
+	tabIconDefault: '#ccc',
+	tabIconSelected: tintColor,
+	tabBar: '#fefefe',
+	errorBackground: 'red',
+	errorText: '#fff',
+	warningBackground: '#EAEB5E',
+	warningText: '#666804',
+	noticeBackground: tintColor,
+	noticeText: '#fff',
 
-  errorBackground: 'red',
-  errorText: '#fff',
+	// global colors
+	editButtonColor: '#011627',
+	titleTodoColor: '#000',
+	detailTodoColor: '#999',
 
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
+	// done todo
+	doneTodoBackground: '#efefef',
+	doneStatusButtonColor: '#59f',
 
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+	// active todo
+	activeTodoBackground: '#FDFFFC',
+	activeStatusButtonColor: '#59f3',
 
-  // Violet: '#9400D3',
-  // Indigo: '#4B0082',
-  // Blue: '#0000FF',
-  // Green: '#00FF00',
-  // Yellow: '#FFFF00',
-  // Orange: '#FF7F00',
-  // Red: '#FF0000',
+	// deleted todo
+	deletedTodoBackground: '#FFF',
+	deletedStatusButtonColor: '#FF1654',
+
+	// buttons
+	doneSwipeButtonBackground: '#70C1B3',
+	doneSwipeButtonColor: '#FFF',
+
+	activeSwipeButtonBackground: '#FDFFFC',
+	activeSwipeButtonColor: '#888',
+
+	deletedSwipeButtonBackground: '#FF1654',
+	deletedSwipeButtonColor: '#fff',
+
+	restoreSwipeButtonBackground: '#59f',
+	restoreSwipeButtonColor: '#fff',
+
+	// Violet: '#9400D3',
+	// Indigo: '#4B0082',
+	// Blue: '#0000FF',
+	// Green: '#00FF00',
+	// Yellow: '#FFFF00',
+	// Orange: '#FF7F00',
+	// Red: '#FF0000',
 };
